52376532da46bf42d289156fc9d7720d76bf70ca
HACO <jasonytonlinecomeandsee@gmail.com>
2022-12-30 22:36:11 +0800

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -2,7 +2,7 @@ import { BsSearch } from "react-icons/bs";
 
 export default function Navbar() {
   return (
-    <div className=" h-[105px] p-5 pt-8 text-black text-2xl shadow-lg flex items-center justify-center">
+    <div className=" h-[105px] p-5 pt-5 text-black text-2xl shadow-lg flex items-center justify-center">
       <div className="flex items-center bg-slate-200 rounded-lg m-5 h-10 flex-auto max-w-[800px]">
         <BsSearch className="text-black ml-3 text-base align-middle cursor-pointer" />
         <input
