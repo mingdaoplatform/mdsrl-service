f1497781d434ba012874e1c5dc8a164c89f25b44
banahaker <pn0818x@outlook.com>
2023-01-01 19:57:54 +0800

    feat: post component

--- components/home/post.js
+++ components/home/post.js
@@ -1,8 +1,9 @@
 import { BsChatLeft } from "react-icons/bs";
+import { AiOutlineHeart } from "react-icons/ai";
 
 export default function Post() {
   return (
-    <div className="p-5 shadow-lg rounded-lg w-full">
+    <div className="mt-10 p-5 shadow-[0_0px_40px_-15px_rgba(0,0,0,0.3)] rounded-lg w-full">
       <h1 className="text-dark-purple text-2xl font-bold">集合小問題</h1>
       <p className="mt-3">
         集合 大致可分為兩類 第一類集合A 就是包含自身的集合 第二類集合B
@@ -11,8 +12,11 @@ export default function Post() {
         屬於A集合的都包含自己但B集合不包含自己所以應該被歸在B集合
         但B集合不屬於自己 ...... (無限循環
       </p>
-      <div className="flex items-center justify-between">
-        <BsChatLeft></BsChatLeft>
+      <div className="flex items-center justify-between mt-3 gap-2 pl-2">
+        <AiOutlineHeart className="text-xl"></AiOutlineHeart>
+        <p className="bg-cyan-600/50 pt-1 pr-2 pb-1 pl-2 text-sm rounded font-semibold	text-cyan-900">
+          Unsolved
+        </p>
       </div>
     </div>
   );
