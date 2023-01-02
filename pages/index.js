f6f4683daab0a6d5107b494ce02b2751886fac53
HACO <jasonytonlinecomeandsee@gmail.com>
2023-01-02 16:23:23 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,47 @@
 import Post from "../components/home/post";
+import React from "react";
+import axios from "axios";
 
 export default function Home() {
+  const [posts, setPosts] = React.useState([]);
+  React.useEffect(() => {
+    axios
+      .get(`/api/forum/post`, {
+        headers: {
+          key: process.env.NEXT_PUBLIC_POSTKEY,
+        },
+      })
+      .then((response) => {
+        if (response.data !== undefined) {
+          setPosts(response.data.data);
+        }
+      });
+  });
+  console.log(posts);
+  const PostHtml = [];
+  if (posts.length === 0) {
+    PostHtml.push(
+      <h1 className="text-center mt-5 text-3xl" key="No Posts">
+        沒有任何討論內容
+      </h1>
+    );
+  } else {
+    for (var i = 0; i < posts.length; i++) {
+      PostHtml.push(
+        <Post
+          title={posts[i].title}
+          content={posts[i].content}
+          solved={posts[i].solved}
+          key={posts[i].id}
+        />
+      );
+    }
+  }
+
   return (
     <>
       <main className=" overflow-auto h-[calc(100vh-105px)]">
-        <div className="flex flex-column p-3">
-          <Post
-            title="集合小問題"
-            content="集合 大致可分為兩類 第一類集合A 就是包含自身的集合 第二類集合B 就是不包含自身的集合 這兩個AB集合把集合分成了兩類 那AB集合要怎麼分類 如果B集合屬於自己 那就代表B集合包含B集合 挖靠那B集合不就屬於A集合 且 屬於A集合的都包含自己但B集合不包含自己所以應該被歸在B集合 但B集合不屬於自己 ...... (無限循環"
-            solved={false}
-          />
-        </div>
-        <div className="flex flex-column p-3">
-          <Post
-            title="集合小問題"
-            content="集合 大致可分為兩類 第一類集合A 就是包含自身的集合 第二類集合B 就是不包含自身的集合 這兩個AB集合把集合分成了兩類 那AB集合要怎麼分類 如果B集合屬於自己 那就代表B集合包含B集合 挖靠那B集合不就屬於A集合 且 屬於A集合的都包含自己但B集合不包含自己所以應該被歸在B集合 但B集合不屬於自己 ...... (無限循環"
-            solved={true}
-          />
-        </div>
-        <div className="flex flex-column p-3">
-          <Post
-            title="集合小問題"
-            content="集合 大致可分為兩類 第一類集合A 就是包含自身的集合 第二類集合B 就是不包含自身的集合 這兩個AB集合把集合分成了兩類 那AB集合要怎麼分類 如果B集合屬於自己 那就代表B集合包含B集合 挖靠那B集合不就屬於A集合 且 屬於A集合的都包含自己但B集合不包含自己所以應該被歸在B集合 但B集合不屬於自己 ...... (無限循環"
-            solved={false}
-          />
-        </div>
-        <div className="flex flex-column p-3">
-          <Post
-            title="集合小問題"
-            content="集合 大致可分為兩類 第一類集合A 就是包含自身的集合 第二類集合B 就是不包含自身的集合 這兩個AB集合把集合分成了兩類 那AB集合要怎麼分類 如果B集合屬於自己 那就代表B集合包含B集合 挖靠那B集合不就屬於A集合 且 屬於A集合的都包含自己但B集合不包含自己所以應該被歸在B集合 但B集合不屬於自己 ...... (無限循環"
-            solved={true}
-          />
-        </div>
+        {PostHtml}
         <br />
       </main>
     </>
