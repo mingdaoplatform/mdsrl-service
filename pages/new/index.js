71f3e800d81f1a23755f0ca6f9b300580c63243b
HACO <jasonytonlinecomeandsee@gmail.com>
2023-01-02 20:43:42 +0800

    Update index.js

--- pages/new/index.js
+++ pages/new/index.js
@@ -4,7 +4,7 @@ import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 export default function New() {
   return (
     <>
-      <main className="p-4 flex items-center flex-col select-none overflow-auto h-[calc(100vh-105px)] ">
+      {/* <main className="p-4 flex items-center flex-col select-none overflow-auto h-[calc(100vh-105px)] w-[calc(100vh-80px)] max-w-[calc(100vh-80px)]">
         <div className="mt-2 p-5 shadow-[0_0px_40px_-15px_rgba(0,0,0,0.3)] rounded-lg w-9/10 lg:w-full h-auto">
           <h1 className="text-dark-purple text-2xl font-bold">新增問題</h1>
           <div className="mt-4 flex gap-1 flex-col lg:flex-row">
@@ -31,23 +31,22 @@ export default function New() {
                 className="rounded-lg border-neutral-500 border-2"
                 id="subject"
               >
-                <option value="1">綜合</option>
-                <option value="2">物理</option>
-                <option value="3">化學</option>
-                <option value="4">生物</option>
-                <option value="5">歷史</option>
-                <option value="6">公民</option>
-                <option value="7">國文</option>
-                <option value="8">地理</option>
-                <option value="9">數學A</option>
-                <option value="10">數學B</option>
-                <option value="11">英文</option>
-                <option value="12">學測</option>
-                <option value="13">分科測驗</option>
-                <option value="14">競賽</option>
-                <option value="26">地科</option>
-                <option value="27">國寫</option>
-                <option value="27">其他</option>
+                <option value="0">綜合</option>
+                <option value="1">物理</option>
+                <option value="2">化學</option>
+                <option value="3">生物</option>
+                <option value="4">歷史</option>
+                <option value="5">公民</option>
+                <option value="6">國文</option>
+                <option value="7">地理</option>
+                <option value="8">數學</option>
+                <option value="9">英文</option>
+                <option value="10">學測</option>
+                <option value="11">分科</option>
+                <option value="12">競賽</option>
+                <option value="13">地科</option>
+                <option value="14">國寫</option>
+                <option value="15">其他</option>
               </select>
             </div>
             <div className="flex flex-col w-4/6">
@@ -83,7 +82,7 @@ export default function New() {
             />
           </div>
         </div>
-      </main>
+      </main> */}
     </>
   );
 }
