04dc2c639990b1ba20f20ad8e11d113ea3fea2c5
HACO <jasonytonlinecomeandsee@gmail.com>
2023-01-03 22:46:53 +0800

    Update post.js

--- components/post.js
+++ components/post.js
@@ -1,24 +1,36 @@
 import parse from "html-react-parser";
+import { useRouter } from "next/router";
 import { BsBookmark } from "react-icons/bs";
 import { AiOutlineHeart } from "react-icons/ai";
 import { FiMessageCircle } from "react-icons/fi";
 
 export default function Post(params) {
+  const router = useRouter();
   return (
-    <div className="mt-2 p-5 shadow-[0_0px_40px_-15px_rgba(0,0,0,0.3)] rounded-lg w-full">
+    <div
+      className="mt-2 p-5 shadow-[0_0px_40px_-15px_rgba(0,0,0,0.3)] rounded-lg w-full noscroll select-none"
+      onClick={() => {
+        router.push(`/post/${params.id}`);
+      }}
+    >
       <h1 className="text-dark-purple text-2xl font-bold">{params.title}</h1>
       <div className="mt-3">{parse(params.content)}</div>
-      <div className="flex items-center justify-between mt-3 gap-2">
-        <div className="flex bg-cyan-600/20 rounded-lg gap-2">
-          <span className="p-2 hover:bg-cyan-600 rounded-lg cursor-pointer transition-colors flex items-center">
+      <div className="flex items-center justify-between mt-4 text-black">
+        <div className="flex rounded-lg">
+          <span className="p-2 rounded-lg transition-colors flex items-center opacity-30">
             <AiOutlineHeart className="text-xl" />
             <p className="ml-1">{params.like}</p>
           </span>
-          <span className="p-2 hover:bg-cyan-600 rounded-lg cursor-pointer transition-colors flex items-center">
+          <span
+            className="p-2 hover:bg-gray-200 rounded-lg cursor-pointer transition-colors flex items-center"
+            onClick={() => {
+              router.push(`/post/${params.id}`);
+            }}
+          >
             <FiMessageCircle className="text-xl" />
             <p className="ml-1">{params.reply}</p>
           </span>
-          <span className="p-2 hover:bg-cyan-600 rounded-lg cursor-pointer transition-colors flex items-center">
+          <span className="p-2 hover:bg-light-white rounded-lg transition-colors flex items-center opacity-30">
             <BsBookmark className="text-xl" />
             <p className="ml-1">{params.collect}</p>
           </span>
