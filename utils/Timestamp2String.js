a22c961a714f50b45213309505b546a2a0d3cd1f
HACO <jasonytonlinecomeandsee@gmail.com>
2023-01-04 18:44:01 +0800

    Update Timestamp2String.js

--- utils/Timestamp2String.js
+++ utils/Timestamp2String.js
@@ -1,4 +1,15 @@
 export const timestamp2string = (timestamp) => {
   const date = new Date(timestamp);
-  return date.getHours() + ":" + date.getMinutes() + ", " + date.toDateString();
+  return (
+    date.getFullYear() +
+    "年" +
+    (parseInt(date.getMonth()) + 1) +
+    "月" +
+    date.getDay() +
+    "日\n" +
+    date.getHours() +
+    "時" +
+    date.getMinutes() +
+    "分"
+  );
 };
